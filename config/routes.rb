90281f578f60a96636a43ba0a3b6d7f55e94a393
Michael Harmer <michaeltharmer@gmail.com>
2023-09-27 13:49:39 -0400

    Added recipes scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@
 
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      resources :recipes
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_052334) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_173506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "ingredients"
+    t.text "instructions"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -37,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_052334) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
